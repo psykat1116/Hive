0787ca681921cdd4798cf30de22f28be561b34c3
Saikat Samanta <ss2519@it.jgec.ac.in>
2026-05-22 16:44:38 +0530

    Update Version

--- proxy.ts
+++ proxy.ts
@@ -17,7 +17,5 @@ export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
 });
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
